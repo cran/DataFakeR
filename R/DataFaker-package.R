ff174573fda51498f41b5fa6e22b807e32c70aa4
Krystian Igras <krystian8207@gmail.com>
2021-09-23 17:10:06 +0000

    version 0.1.2

---
+++ R/DataFaker-package.R
@@ -0,0 +1,12 @@
+#' Generate fake data based on provided schema specification
+#'
+#' @name DataFakeR-package
+#' @importFrom magrittr %>%
+
+globalVariables(c(
+  ":=", "!!", ".", "from", "to", ".data", "nodes",
+  "nchars", "vals", "val", "constraint_type", "table_name",
+  "unique_constraint_name", "column_name", "name"
+))
+
+NULL
